4bee96a085643f5b20b79f67f6d9e299c2cb3c56
Mohanty <ayushman.mohanty@capgemini.com>
2024-12-06 10:38:12 +0530

    Initial Commit

---
+++ Case_1/promotionfinder/src/main/java/com/backend/promotionfinder/Controller/PromotionController.java
@@ -0,0 +1,42 @@
+package com.backend.promotionfinder.Controller;
+
+import com.backend.promotionfinder.Promotion;
+import com.backend.promotionfinder.service.PromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PromotionController {
+
+    @Autowired
+    private PromotionService promotionService; // Service to fetch promotions from the database
+
+    // Endpoint to get all promotions
+    @GetMapping("/promotions")
+    public List<Promotion> getPromotions() {
+        return promotionService.getAllPromotions();
+    }
+
+    // Endpoint to get promotions by country
+    @GetMapping("/promotions/{country}")
+    public List<Promotion> getPromotionsByCountry(@PathVariable String country) {
+        return promotionService.getPromotionsByCountry(country);
+    }
+
+    // Endpoint to get list of countries
+    @GetMapping("/countries")
+    public List<String> getCountries() {
+        return promotionService.getAllCountries();
+    }
+    
+    
+}
+
+
+
+
+
